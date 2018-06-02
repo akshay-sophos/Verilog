b0bfedf4c8ea4044161f6f83ffcf3a167123d86c
Akshay Sreekumar <akshaysreekumar1997@gmail.com>
2018-06-02 20:08:43 +0530

    Test Bench for Half Adder

---
+++ half_adder_test.v
@@ -0,0 +1,16 @@
+module test;
+reg x,y;
+wire c,s;
+half_adder DUT(s,c,x,y);
+initial
+begin
+$dumpfile("half_adder.vcd");
+$dumpvars(0,test);
+$monitor($time,": x= %b y= %b s = %b c = %b",x,y,s,ca);
+#5 x = 0; y=0;
+#5 x = 1; y=1;
+#5 x= 1; y = 0;
+#5 x=0; y=1;
+#5 $finish;
+end
+endmodule
